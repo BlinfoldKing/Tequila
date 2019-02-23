a9fedf545a92022d33aad9ae59a08ea43c42eb41
Ganesha Danu <ganeshadanu1@gmail.com>
2019-02-23 23:52:27 +0700

    add input prompt

--- src/main/java/Tequila/Main.java
+++ src/main/java/Tequila/Main.java
@@ -1,7 +1,24 @@
 package Tequila;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Scanner;
+
 public class Main {
+    static Scanner input = new Scanner(System.in);
+    
+    static String getInput() {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        System.out.print("["+sdf.format(cal.getTime())+"]$ ");
+        return input.nextLine();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String query = getInput();
+        while(!query.equals("exit")) {
+            System.out.println("output: "+query.length());
+            query = getInput();
+        } 
     }
 }
