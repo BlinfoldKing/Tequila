4c2f9019e98eef2b1f5758851d5bb3db6b5d8c3e
Eko Hardiyanto <ehardi19@gmail.com>
2019-02-24 18:30:32 +0700

    add path to menulis.csv

--- src/main/java/Tequila/entity/Penulis.java
+++ src/main/java/Tequila/entity/Penulis.java
@@ -3,24 +3,24 @@ package Tequila.entity;
 public class Penulis {
     public String kode_penulis;
     public String nama;
+    public String tahun_lahir;
     public String alamat;
     public String no_hp;
     public String email;
-    public String tahun_lahir;
 
     public Penulis(
         String kode_penulis,
         String nama,
+        String tahun_lahir,
         String alamat,
         String no_hp,
-        String email,
-        String tahun_lahir
+        String email
     ) {
         this.kode_penulis = kode_penulis;
         this.nama = nama;
+        this.tahun_lahir = tahun_lahir;
         this.alamat = alamat;
         this.no_hp = no_hp;
         this.email = email;
-        this.tahun_lahir = tahun_lahir;
     }
 }
